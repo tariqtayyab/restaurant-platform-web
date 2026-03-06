0cc3365cb3e3e415f8d10530d92d09686047db73
tariqtayyab <tariqtayyab229@gmail.com>
2026-03-06 23:02:01 +0500

    test 6

--- restaurant-platform/components/search/SearchClient.tsx
+++ restaurant-platform/components/search/SearchClient.tsx
@@ -51,7 +51,7 @@ export default function SearchClient({ restaurant, subdomain, initialQuery }: Se
     const fetchMenu = async () => {
       try {
         setLoading(true)
-        const API_BASE_URL = process.env.NEXT_PUBLIC_API_BASE_URL || 'https://localhost:7026'
+        const API_BASE_URL = 'http://34.235.60.49'
         const response = await fetch(`${API_BASE_URL}/api/public/website/${subdomain}/menu`, {
           headers: { 'accept': '*/*' }
         })
